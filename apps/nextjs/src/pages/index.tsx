2b1321f08a9fbec9ed96e91575b22dcaee509f4f
Sander <mail@sanderboer.nl>
2023-10-31 09:53:12 +0100

    fix: remove old endpoint

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -57,24 +57,11 @@ export default Home;
 
 const AuthShowcase: React.FC = () => {
   const { isSignedIn } = useAuth();
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery(
-    undefined,
-    { enabled: !!isSignedIn },
-  );
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       {isSignedIn && (
         <>
-          <p className="text-center text-2xl text-white">
-            {secretMessage && (
-              <span>
-                {" "}
-                {secretMessage} click the user button!
-                <br />
-              </span>
-            )}
-          </p>
           <div className="flex items-center justify-center">
             <UserButton
               appearance={{
